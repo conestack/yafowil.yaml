12b089b8ba655e7509699bbd5e72200aaefdb1e8
Robert Niederreiter <rnix@squarewave.at>
2011-06-04 14:00:41 +0200

    back to dev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.9.1'
+version = '0.9.2dev'
 shortdesc = \
 'YAFOWIL - YAML parser for widget trees.'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
